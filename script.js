c6daa2166a1a72dc888fead70f3e0ca98595fdce
Britteany <miszbritteany@gmail.com>
2023-11-22 19:21:09 -0500

    Tasks are displayed on the page

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const openForm = document.querySelector(".open");
 openForm.addEventListener("click", function () {
     document.querySelector(".form-popup").style.display = "block";
 });
+
 //Cancel and Submit Form
 const reset = document.querySelector(".form-container")
 const closeForm = document.querySelector(".cancel")
@@ -13,43 +14,88 @@ closeForm.addEventListener("click", function () {
     reset.reset();
 });
 
-addTask.addEventListener("click", function () {
+addTask.addEventListener("click", function (event) {
     event.preventDefault();
     let taskInfo = taskValue();
+    displayTask(taskInfo);
     document.querySelector(".form-popup").style.display = "none";
     reset.reset();
 });
+
 //Access Tasks Dropdown Open/Close on Click In/Out
 const viewTaskbtn = document.querySelector(".dropbtn")
 const viewTask = document.querySelector(".dropdown-content")
 
 viewTaskbtn.addEventListener("click", function () {
-    if(viewTask.style.display === "block"){
+    if (viewTask.style.display === "block") {
         viewTask.style.display = "none";
     } else {
         viewTask.style.display = "block";
     }
 });
 
-document.addEventListener("click", function(event){
-    if(event.target !== viewTaskbtn && !viewTask.contains(event.target)) {
+document.addEventListener("click", function (event) {
+    if (event.target !== viewTaskbtn && !viewTask.contains(event.target)) {
         viewTask.style.display = "none";
     }
 });
 
 //Get Tasks Values
-function taskValue() {
-    let cats = document.getElementById("#category").value;
-    let title = document.getElementById("#title").value;
-    let descript = document.getElementById("#description").value;
-    let date = document.getElementById("#date").value;
-    let priority = document.querySelector(".p-value").value;
-    let notes = documcument.getElementById("#notes").value;
+function Task(category, title, description, date, priority, notes) {
+    this.category = category;
+    this.title = title;
+    this.description = description;
+    this.date = date;
+    this.priority = priority;
+    this.notes = notes;
+};
 
-    let taskInfo = new task(cats, title, descript, date, priority, notes);
+function taskValue() {
+    const category = document.getElementById("category").value;
+    const title = document.getElementById("title").value;
+    const description = document.getElementById("description").value;
+    const date = document.getElementById("date").value;
+    const notes = document.getElementById("notes").value;
+    const priority = document.getElementById("p-type").value;
+
+    const taskInfo = new Task(category, title, description, date, priority, notes);
 
     return taskInfo;
-}
+};
+
+//Display Tasks on Page
+function displayTask(taskInfo) {
+    const taskList = document.getElementById("task-list");
+    const taskDiv = document.createElement("div");
+    taskDiv.classList.add("task-style");
+    const catP = document.createElement("p");
+    catP.classList.add("cat-task");
+    catP.textContent = `Category: ${taskInfo.category}`;
+    const titleP = document.createElement("p");
+    titleP.classList.add("title-task");
+    titleP.textContent = `Title: ${taskInfo.title}`;
+    const descriptP = document.createElement("p");
+    descriptP.classList.add("descript-task");
+    descriptP.textContent = `Description: ${taskInfo.description}`;
+    const dateP = document.createElement("p");
+    dateP.classList.add("date-task");
+    dateP.textContent = `Due Date: ${taskInfo.date}`;
+    const priorityP = document.createElement("p");
+    priorityP.classList.add("priority-task");
+    priorityP.textContent = `Priority: ${taskInfo.priority}`;
+    const notesP = document.createElement("p");
+    notesP.classList.add("notes-task");
+    notesP.textContent = `Notes: ${taskInfo.notes}`;
+
+    taskDiv.appendChild(catP);
+    taskDiv.appendChild(titleP);
+    taskDiv.appendChild(descriptP);
+    taskDiv.appendChild(dateP);
+    taskDiv.appendChild(priorityP);
+    taskDiv.appendChild(notesP);
+
+    taskList.appendChild(taskDiv);
+};
 
 
 
